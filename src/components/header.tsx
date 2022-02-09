21b2ac667cb4478a68e95e7ea7782dfb16d7b23e
shojs777 <shokiebi2561@gmail.com>
2022-02-09 22:41:08 +0900

    改造直前

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,9 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
+  { label: 'Blog', page: '/' },
+  { label: 'Plofile', page: '/plofile' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
